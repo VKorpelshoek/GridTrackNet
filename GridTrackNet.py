ebae6eb87f4eaaa66d4cb0972431b7447dccb313
VKorpelshoek <v.korpelshoek@hotmail.com>
2023-06-02 10:57:46 +0200

    original tracknet

--- GridTrackNet.py
+++ GridTrackNet.py
@@ -2,36 +2,70 @@ from keras.models import *
 from keras.layers import *
 from keras.activations import *
 
-def conv_layer(inputs, filters, kernel_size):
-    x = Conv2D(filters, kernel_size, padding='same', data_format='channels_first')(inputs)
-    x = BatchNormalization()(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    return x
-
-
 def GridTrackNet(imgs_per_instance, input_height, input_width ):
-		
+
 	imgs_input = Input(shape=(imgs_per_instance*3,input_height,input_width))
 
-	x = conv_layer(imgs_input, 64, (3,3))
+	x = Conv2D(64, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first' )(imgs_input)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
+
+	x = Conv2D(64, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first' )(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
 
 	x = MaxPooling2D((2, 2), strides=(2, 2), data_format='channels_first' )(x)
 
-	x = conv_layer(x, 128, (3,3))
+	x = Conv2D(128, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first' )(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
+
+	x = Conv2D(128, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first' )(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
 
 	x = MaxPooling2D((2, 2), strides=(2, 2), data_format='channels_first' )(x)
 
-	x = conv_layer(x, 256, (3,3))
+	x = Conv2D(256, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first' )(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
+
+	x = Conv2D(256, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first' )(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
 
 	x = MaxPooling2D((2, 2), strides=(2, 2), data_format='channels_first' )(x)
 
-	x = conv_layer(x, 512, (3,3))
+	x = ( Conv2D(256, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first'))(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
+
+	x = ( Conv2D(256, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first'))(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
+
+	x = ( Conv2D(256, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first'))(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
 
 	x = MaxPooling2D((2, 2), strides=(2, 2), data_format='channels_first' )(x)
 
-	output = Conv2D(imgs_per_instance*3, (3,3), activation='sigmoid', data_format='channels_first', padding='same')(x)
+	x = ( Conv2D(512, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first'))(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
 
-	model = Model(imgs_input, output)
+	x = ( Conv2D(512, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first'))(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
+
+	x = ( Conv2D(512, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first'))(x)
+	x = ( Activation('relu'))(x)
+	x = ( BatchNormalization())(x)
+
+	x = ( Conv2D(imgs_per_instance*3, (3, 3), kernel_initializer='random_uniform', padding='same', data_format='channels_first'))(x)
+	output = ( Activation('sigmoid'))(x)
+
+	model = Model( imgs_input , output)
 
 	return model
+
